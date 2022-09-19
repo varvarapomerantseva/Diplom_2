b08bc09f9a7de08bc862b4db6425a7457450b755
Varya Pomerantseva <vb1503@yandex.ru>
2022-09-20 01:11:57 +0300

    add allure report

---
+++ src/main/java/Ingredients.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Ingredients {
+
+    public static final List<String> VALID = List.of("61c0c5a71d1f82001bdaaa79","61c0c5a71d1f82001bdaaa76");
+
+    public static final List<String> INVALID = List.of("60c0c5a71d1f82001bdaaa79","60c0c5a71d1f82001bdaaa76");
+}
